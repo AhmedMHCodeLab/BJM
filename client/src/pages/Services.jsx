17e1388d9a9b701a0f6ba609ecefb05604e6dfb0
AhmedMHCodeLab <ahmedmh03.work@gmail.com>
2025-04-09 10:53:36 +0800

    fix: services mobile view (how we operrate)

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -4,7 +4,7 @@ import { services, solutions } from '../utils/constants'
 
 const Services = () => {
   return (
-    <Container fluid px={32} maw="80rem" py={64}>
+    <Container fluid px={{ base: 16, sm: 32 }} maw="80rem" py={{ base: 40, md: 64 }}>
       <SectionTitle title="our services" />
       <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} spacing="lg" pt={32} pb={64}>
         {services.map((service, index) => {
@@ -12,15 +12,15 @@ const Services = () => {
         })}
       </SimpleGrid>
       <SectionTitle title="how we operate" />
-      <Grid gutter="xl">
-        <Grid.Col span={6}>
+      <Grid gutter={{ base: "md", sm: "xl" }}>
+        <Grid.Col span={{ base: 12, md: 6 }}>
           <Image
             w="100%"
             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
             fit="cover"
           />
         </Grid.Col>
-        <Grid.Col span={6}>
+        <Grid.Col span={{ base: 12, md: 6 }}>
           <ServicesAccordion />
         </Grid.Col>
       </Grid>
